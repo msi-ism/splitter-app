b1aa536535fe3c9bc0626663ea5e2b13ba71925f
msi-ism <msism720@gmail.com>
2023-06-02 23:05:52 -0400

    implemented custom tip calc on enter press

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ console.log('script running')
 // Getting elements
 const billAmount = document.querySelector('.dollar-amt')
 const people = document.querySelector('.ppl-input')
+const customTipInput = document.querySelector('.custom-tip')
 const tipAmt = document.querySelector('.tip-amt')
 const totalDOM = document.querySelector('.total-amt')
 const errorText = document.querySelector('.error-text')
@@ -16,6 +17,9 @@ let userPeople;
 let ppAmount;
 let tipOwed;
 let totalAmountPP;
+let customTip;
+
+
 
 const getValue = () => {
     userBillInput = billAmount.value
@@ -37,6 +41,16 @@ const getTotalAmount = () => {
     }
 }
 
+const getCustomTip = () => {
+    customTip = parseInt(customTipInput.value) / 100
+    console.log(customTip)
+}
+
+billAmount.addEventListener('input', getValue)
+people.addEventListener('input', getPeople)
+customTipInput.addEventListener('input', getCustomTip)
+
+
 const validateBill = () => {
     if (billAmount.value >= 1) {
         return true
@@ -47,8 +61,7 @@ const validateBill = () => {
     }
 }
 
-billAmount.addEventListener('input', getValue)
-people.addEventListener('input', getPeople)
+
 
 const calculateTip = (id) => {
     if (billAmount.value < 1) {
@@ -74,6 +87,30 @@ const calculateTip = (id) => {
     }
 }
 
+const calculateCustomTip = (id) => {
+    if (billAmount.value < 1) {
+        errorText.style.display = 'flex'
+        errorText.textContent = "Can't Be Zero"
+        console.log('should be errors')
+        billAmount.style.border = '2px solid rgb(229, 166, 89)'
+
+    }
+    if (people.value < 1) {
+        peopleErrorText.style.display = 'flex'
+        peopleErrorText.textContent = "Can't Be Zero"
+        console.log('should be errors')
+        people.style.border = '2px solid rgb(229, 166, 89)'
+
+    } else if (billAmount.value > 0 && userPeople > 0) {
+        let tipPercent = parseInt(id) / 100
+        tipOwed = userBillInput * customTip
+        ppAmount = tipOwed / userPeople
+        getTotalAmount()
+        tipAmt.textContent = `$ ${parseInt(tipOwed / userPeople).toFixed(2)}`
+        totalDOM.textContent = `$ ${totalAmountPP.toFixed(2)}`
+    }
+}
+
 
 
 tipBtns.forEach(button => {
@@ -81,5 +118,15 @@ tipBtns.forEach(button => {
         calculateTip(button.id)
     })
 
+})
+
+
+document.addEventListener('keypress', (e) => {
+    let name = e.key
+    let code = e.code
+    console.log(e.key)
+    if (name === 'Enter') {
+        calculateCustomTip()
+    }
 })
 
