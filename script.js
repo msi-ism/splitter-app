47be4f48f6a7fa7c44f6579a52ff16715c763f1d
msi-ism <msism720@gmail.com>
2023-06-02 22:15:33 -0400

    validation error text added

--- script.js
+++ script.js
@@ -5,26 +5,26 @@ const billAmount = document.querySelector('.dollar-amt')
 const people = document.querySelector('.ppl-input')
 const tipAmt = document.querySelector('.tip-amt')
 const totalDOM = document.querySelector('.total-amt')
+const errorText = document.querySelector('.error-text')
+const peopleErrorText = document.querySelector('.ppl-error-text')
 
 
 console.log(people)
 let tipBtns = document.querySelectorAll('.tip-btn')
 let userBillInput;
-let userPeople = 1;
+let userPeople;
 let ppAmount;
 let tipOwed;
 let totalAmountPP;
 
 const getValue = () => {
     userBillInput = billAmount.value
+    errorText.style.display = 'none'
 }
 
 const getPeople = () => {
-    if (people.value > 1) {
         userPeople = people.value
-    } else {
-        userPeople = 1
-    }
+        peopleErrorText.style.display = 'none'
     console.log(userPeople)
 }
 
@@ -35,31 +35,50 @@ const getTotalAmount = () => {
     } else {
         totalAmountPP = (parseInt(userBillInput) + parseInt(tipOwed)) / 1
     }
+}
 
+const validateBill = () => {
+    if (billAmount.value >= 1) {
+        return true
+    } else {
+        errorText.style.display = 'flex'
+        errorText.textContent = "Can't Be Zero"
+        return false
+    }
 }
 
 billAmount.addEventListener('input', getValue)
+people.addEventListener('input', getPeople)
 
 const calculateTip = (id) => {
-    let tipPercent = parseInt(id) / 100
-    tipOwed = userBillInput * tipPercent
-    ppAmount = tipOwed / userPeople
-    console.log('people amount' + ppAmount)
-    console.log(userBillInput)
-    console.log(tipPercent)
-    console.log(tipOwed)
+    if (billAmount.value < 1) {
+        errorText.style.display = 'flex'
+        errorText.textContent = "Can't Be Zero"
+        console.log('should be errors')
+        billAmount.style.border = '2px solid rgb(229, 166, 89)'
 
+    }
+    if (people.value < 1) {
+        peopleErrorText.style.display = 'flex'
+        peopleErrorText.textContent = "Can't Be Zero"
+        console.log('should be errors')
+        people.style.border = '2px solid rgb(229, 166, 89)'
+
+    } else if (billAmount.value > 0 && userPeople > 0) {
+        let tipPercent = parseInt(id) / 100
+        tipOwed = userBillInput * tipPercent
+        ppAmount = tipOwed / userPeople
+        getTotalAmount()
+        tipAmt.textContent = `$ ${parseInt(tipOwed / userPeople).toFixed(2)}`
+        totalDOM.textContent = `$ ${totalAmountPP.toFixed(2)}`
+    }
 }
 
+
+
 tipBtns.forEach(button => {
-    console.log(button.textContent)
     button.addEventListener('click', () => {
         calculateTip(button.id)
-        getPeople()
-        getTotalAmount()
-        tipAmt.textContent = `$ ${parseInt(tipOwed / userPeople)}`
-        totalDOM.textContent = `$${totalAmountPP}`
-
     })
 
 })
